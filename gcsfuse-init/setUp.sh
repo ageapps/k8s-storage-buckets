d5494a1fe4f44105a48544c29d7c0d95c73c5afd
ageapps <age.apps.dev@gmail.com>
2017-05-30 00:47:03 +0200

    Fix copy command

--- gcsfuse-init/setUp.sh
+++ gcsfuse-init/setUp.sh
@@ -5,5 +5,5 @@ mkdir -p $DESTINATION_DIR
 gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
 echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
 ls $FUSE_MOUNT_DIR
-cp -avr  $FUSE_MOUNT_DIR/ $DESTINATION_DIR/
+cp -avr  $FUSE_MOUNT_DIR/* $DESTINATION_DIR/
 ls $DESTINATION_DIR
