f6a6cc981d87d54a31bb0e75661c92a4f48035a9
ageapps <age.apps.dev@gmail.com>
2017-05-29 18:23:55 +0200

    Added init gcsfuse

--- gcs-fuse-sync/setUp.sh
+++ gcs-fuse-sync/setUp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash          
 
 mkdir -p $FUSE_MOUNT_DIR
+mkdir -p $DESTINATION_DIR
 gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
 echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
 ls $FUSE_MOUNT_DIR
-
+tail -f /dev/null
---
+++ gcsfuse-init/setUp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash          
+
+mkdir -p $FUSE_MOUNT_DIR
+mkdir -p $DESTINATION_DIR
+gcsfuse -o allow_other $FUSE_BUCKET_NAME $FUSE_MOUNT_DIR
+echo "Mounted succesfully FUSE bucket $FUSE_BUCKET_NAME in $FUSE_MOUNT_DIR"
+ls $FUSE_MOUNT_DIR
+cp -ar  $FUSE_MOUNT_DIR/ $DESTINATION_DIR/
+ls $DESTINATION_DIR
